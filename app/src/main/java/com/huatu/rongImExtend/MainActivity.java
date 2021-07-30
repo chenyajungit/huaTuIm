e827610c1ff56b0d54f780be5b099b4ad6a85fcb
chenyajun <chenyajun@huatu.com>
2021-07-30 08:25:12 +0800

    first commit

---
+++ app/src/main/java/com/huatu/rongImExtend/MainActivity.java
@@ -0,0 +1,14 @@
+package com.huatu.rongImExtend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
